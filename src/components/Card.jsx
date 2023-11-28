33719f182a4663f10c17009980f4f9bc29d93610
Atul Singh <atulsingh930193@gmail.com>
2023-11-28 17:34:01 +0530

    first commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -12,7 +12,7 @@ function Card({profileCard, setProfileCard, selectedKey, setShowProfile}) {
     }
   return (
     <div onClick={toggleClick} key={profileCard?.id} className={`${selectedKey === profileCard?.id ? 'bg-neutral-50 lg:scale-105' : ''} flex items-center gap-4 rounded-lg bg-white w-full px-6 py-6 relative transition-all duration-200 shadow-[0px_3px_20px_1px_rgba(212,_212,_212,_1)] lg:hover:scale-105 cursor-pointer box-border`}>
-        <img onError={()=>setImageProfile(profileImage)} className='rounded-full w-20' src={imageProfile} alt="user-image" />
+        <img onError={()=>setImageProfile(profileImage)} className='rounded-full w-20' src={imageProfile} alt="userImage" />
         <div className='flex flex-col items-start'>
             <p className='font-semibold text-lg text-[#13142b]'>{`${profileCard?.profile?.firstName} ${profileCard?.profile?.lastName}`}</p>
             <p className='text-sm text-[#93939e]'>{`@ ${profileCard?.profile?.username}`}</p>
